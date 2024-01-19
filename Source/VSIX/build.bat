d3fbdba3ed4b3a02f828a92de3473be95cc9be51
Brian Kesecker <brian.kesecker@relativity.com>
2024-01-19 12:33:20 -0500

    Comments

--- Source/VSIX/build.bat
+++ Source/VSIX/build.bat
@@ -1,4 +1,6 @@
 %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat"
 nuget restore RelativityWizard\packages.config -SolutionDir .
+REM Calling MSBuild directly does not work.
 REM msbuild.exe AllRelativityTemplates.sln -t:Rebuild -p:Configuration=Debug
+REM https://github.com/dotnet/msbuild/issues/2653
 dotnet msbuild -t:Rebuild -p:Configuration=Debug AllRelativityTemplates.sln
